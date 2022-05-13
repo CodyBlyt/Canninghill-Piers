ab297af96f51299b543ea0adc1c31e8b5934ce3a
Codyblyt <cody@limomaxi.com>
2022-05-13 14:05:21 +0800

    Test

--- pages/project-details.vue
+++ pages/project-details.vue
@@ -3,7 +3,7 @@
     <div>
       <div class="py-12 select-none bg-theme-700">
         <div class="text-xl font-semibold text-center text-white sm:text-4xl">
-          Test
+          Testt
         </div>
       </div>
       <section class="py-12">
